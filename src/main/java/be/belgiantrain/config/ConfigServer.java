077d6c05aa3cffa24de8faad5decc530f83d360f
EXN198 <michael.couck@belgiantrain.be>
2019-07-23 12:00:17 +0200

    Removed the ssl configuration, back to http.

---
+++ src/main/java/be/belgiantrain/config/ConfigServer.java
@@ -0,0 +1,26 @@
+package be.belgiantrain.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * <pre>
+ *     1) De configuration server
+ * </pre>
+ */
+@RefreshScope
+@EnableDiscoveryClient
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@ComponentScan({"be.belgiantrain.config"})
+public class ConfigServer {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ConfigServer.class, args);
+    }
+
+}
